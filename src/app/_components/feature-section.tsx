4833f1bd79189ba34c2701eef591ba2370fdcbfc
Ayrton Junior <xk.junior63@gmail.com>
2024-02-03 20:31:10 -0300

    Style: adding news stack icons

--- src/app/_components/feature-section.tsx
+++ src/app/_components/feature-section.tsx
@@ -7,7 +7,14 @@ import {
   Pgsql,
   Mysql,
   PhpLaravel,
-  NodeJs
+  NodeJs,
+  ZendFrameWork,
+  MongoDB,
+  Azure,
+  Docker,
+  Ubuntu,
+  AngularJs,
+  Python
 } from '@/components/stack-icons';
 
 const content: [] = [];
@@ -16,7 +23,7 @@ function FeatureSection() {
   return (
     <section
       id='feature'
-      className='bg-slate-50 dark:bg-transparent container space-y-6 py-8 md:py-12 lg:py-24'
+      className='bg-slate-50 dark:bg-transparent container space-y-3 py-8 md:py-12 lg:py-24'
     >
       <div className='mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center'>
         <h2 className='font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl'>
@@ -27,17 +34,26 @@ function FeatureSection() {
         </p>
       </div>
 
-      <div className='mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-auto md:grid-cols-4'>
+      <div className='mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-auto md:grid-cols-3'>
         <div className='flex h-[100px] flex-row gap-6 border-b-[2px] border-indigo-200 fill-current'>
           <Php />
           <PhpLaravel />
-          <Pgsql />
+          <ZendFrameWork />
           <Mysql />
+          <Pgsql />
         </div>
         <div className='flex h-[100px] flex-row gap-6 border-b-[2px] border-indigo-200 fill-current'>
-          <NextJs />
           <React />
+          <NextJs />
+          <AngularJs />
           <NodeJs />
+          <MongoDB />
+        </div>
+        <div className='flex h-[100px] flex-row gap-6 border-b-[2px] border-indigo-200 fill-current'>
+          <Python />
+          <Azure />
+          <Docker />
+          <Ubuntu />
         </div>
       </div>
       {content.length > 0 ?? <AccordeonSection content={content} />}
